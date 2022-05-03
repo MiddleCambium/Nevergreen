6a73b9b59484fd5063028934e7d3f7421af177df
Ste Smith <ste.smith@oriium.com>
2022-05-03 14:08:36 +0100

    Updated SMART Product Name

--- Nevergreen/Apps/Get-SMARTProductDriversAndInk.ps1
+++ Nevergreen/Apps/Get-SMARTProductDriversAndInk.ps1
@@ -2,10 +2,17 @@
 # Link to the main download page https://support.smarttech.com/docs/software/product-drivers-ink/12-5/en/downloads/drink-windows-download.cshtml
 # link address on the download webpage "https://downloads.smarttech.com/software/driversink/12_18/smartproductdrivers12_18ink5_11.exe"
 
-$URL = Get-Link -Uri 'https://support.smarttech.com/docs/software/product-drivers-ink/12-5/en/downloads/drink-windows-download.cshtml' -MatchProperty href -Pattern '.exe'
-#URL returns https://downloads.smarttech.com/software/driversink/12_18/smartproductdrivers12_18ink5_11.exe
+try {
 
-$Version = Get-Version -Uri 'https://support.smarttech.com/docs/software/product-drivers-ink/12-5/en/downloads/drink-windows-download.cshtml' -Pattern 'SMART&#160;Product Drivers: ((?:\d+\.)+(\d+\.)+(\d+\.)+(\d+))'
-# $Version returns 12.18.245.0
+    $URL = Get-Link -Uri 'https://support.smarttech.com/docs/software/product-drivers-ink/12-5/en/downloads/drink-windows-download.cshtml' -MatchProperty href -Pattern '.exe'
+    #URL returns https://downloads.smarttech.com/software/driversink/12_18/smartproductdrivers12_18ink5_11.exe
 
-New-NevergreenApp -Name 'SMART Product Drivers' -Version $Version -Uri $URL -Architecture 'x86' -Type 'Exe'
+    $Version = Get-Version -Uri 'https://support.smarttech.com/docs/software/product-drivers-ink/12-5/en/downloads/drink-windows-download.cshtml' -Pattern 'SMART&#160;Product Drivers: ((?:\d+\.)+(\d+\.)+(\d+\.)+(\d+))'
+    # $Version returns 12.18.245.0
+
+    New-NevergreenApp -Name 'SMART Product Drivers And Ink' -Version $Version -Uri $URL -Architecture 'x86' -Type 'Exe'
+
+}
+catch {
+    Write-Error "$($MyInvocation.MyCommand): $($_.Exception.Message)"
+}
